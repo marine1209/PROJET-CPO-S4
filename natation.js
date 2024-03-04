22e9b9f8f28bcf70b93f31b591e22030afa2edfd
marie bertrand <marie.bertrand@epfedu.fr>
2024-03-04 12:44:51 +0100

    creation pyrahna

--- natation.js
+++ natation.js
@@ -1,55 +1,43 @@
-/***********************************************************************/
-/** CONFIGURATION GLOBALE DU JEU ET LANCEMENT 
-/***********************************************************************/
+var player; //designe le sprite du joueur
+var clavier; //pour la gestion du clavier
+var groupe_pyrahnas; // contient tous les sprite étoiles
 
-// configuration générale du jeu
-var config = {
-    type: Phaser.AUTO,
-    width: 800, // largeur en pixels
-    height: 600, // hauteur en pixels
-    physics: {
-      // définition des parametres physiques
-      default: "arcade", // mode arcade : le plus simple : des rectangles pour gérer les collisions. Pas de pentes
-      arcade: {
-        // parametres du mode arcade
-        gravity: {
-          y: 300 // gravité verticale : acceleration ddes corps en pixels par seconde
-        },
-        debug: false // permet de voir les hitbox et les vecteurs d'acceleration quand mis à true
-      }
-    },
-    scene: {
-      // une scene est un écran de jeu. Pour fonctionner il lui faut 3 fonctions  : create, preload, update
-      preload: preload, // la phase preload est associée à la fonction preload, du meme nom (on aurait pu avoir un autre nom)
-      create: create, // la phase create est associée à la fonction create, du meme nom (on aurait pu avoir un autre nom)
-      update: update // la phase update est associée à la fonction update, du meme nom (on aurait pu avoir un autre nom)
+export default class nnatation2 extends Phaser.Scene {
+    // constructeur de la classe
+    constructor() {
+      super({
+        key: "natation" //  ici on précise le nom de la classe en tant qu'identifiant
+      });
     }
-  };
 
-  // création et lancement du jeu
-new Phaser.Game(config);
 /***********************************************************************/
-/** FONCTION PRELOAD 
+/** METHODE PRELOAD 
 /***********************************************************************/
 
 /** La fonction preload est appelée une et une seule fois,
  * lors du chargement de la scene dans le jeu.
  * On y trouve surtout le chargement des assets (images, son ..)
  */
-function preload() {
-    this.load.image("img_sky", "src/assets/sky.png");
-    this.load.image("img_plateform", "src/assets/platform.png");
-    this.load.image("img_etoile", "src/assets/star.png"); 
-    this.load.image("img_bombe", "src/assets/bomb.png"); 
-    this.load.spritesheet("img_perso", "src/assets/dude.png", {
-      frameWidth: 32,
-      frameHeight: 48
+preload() {
+
+    this.load.image("img_pyrhana", "src/assets/pyrahna.png"); 
+    this.load.spritesheet("img_perso", "src/assets/sportif.png", {
+      frameWidth: 48,
+      frameHeight: 72
     });
-  }
+
+    // chargement tuiles de jeu
+    this.load.image("tuiles_natation", "src/assets/image_natation/terrain_atlas.png");
+    this.load.image("tuiles_natation2", "src/assets/image_natation/base_out_atlas.png");
+
+    // chargement de la carte
+    this.load.tilemapTiledJSON("carte_natation", "src/assets/image_natation/map_natation.tmj");  
+}
+  
 
 
 /***********************************************************************/
-/** FONCTION CREATE 
+/** METHODE CREATE 
 /***********************************************************************/
 
 /* La fonction create est appelée lors du lancement de la scene
@@ -58,22 +46,40 @@ function preload() {
  * placement des peronnages, des sprites, des platesformes, création des animations
  * ainsi que toutes les instructions permettant de planifier des evenements
  */
-  function create() {
-    clavier = this.input.keyboard.createCursorKeys();
-  
-     // dans cette partie, on crée les animations, à partir des spritesheet
+ create() {
+
+/** CREATION DU PERSONNAGE  ET ENNEMIS **/
+// On créée un nouveau personnage : player et on le positionne
+player = this.physics.add.sprite(100, 450, "img_perso")
+//  propriétées physiqyes de l'objet player :
+this.player.setBounce(0.2); // on donne un petit coefficient de rebond
+this.player.setCollideWorldBounds(true); // le player se cognera contre les bords du monde
+this.player.setDepth(50);
+
+this.add.image(800,500,"img_pyrahna");
+groupe_pyrahnas=this.physics.add.groupe();
+this.physics.add.collider(groupe_pyrahnas, claque_plateforme);
+
+this.physics.add.collider(player, groupe_pyrahnas, chocAvecPyrahna, null, this);
+for (var i = 0; i < 10; i++) {
+    var coordX = 70 + 70 * i;
+    groupe_etoiles.create(coordX, 10, "img_etoile");
+    this.physics.add.collider(groupe_etoiles, groupe_plateformes); 
+  } 
+/** ANIMATIONS **/
+  // dans cette partie, on crée les animations, à partir des spritesheet
   // chaque animation est une succession de frame à vitesse de défilement défini
   // une animation doit avoir un nom. Quand on voudra la jouer sur un sprite, on utilisera la méthode play()
   // creation de l'animation "anim_tourne_gauche" qui sera jouée sur le player lorsque ce dernier tourne à gauche
   this.anims.create({
     key: "anim_tourne_gauche", // key est le nom de l'animation : doit etre unique poru la scene.
-    frames: this.anims.generateFrameNumbers("img_perso", { start: 0, end: 3 }), // on prend toutes les frames de img perso numerotées de 0 à 3
+    frames: this.anims.generateFrameNumbers("img_perso", { start: 15, end: 17 }), // on prend toutes les frames de img perso numerotées de 0 à 3
     frameRate: 10, // vitesse de défilement des frames
     repeat: -1 // nombre de répétitions de l'animation. -1 = infini
   }); 
   this.anims.create({
     key: "anim_tourne_droite", // key est le nom de l'animation : doit etre unique poru la scene.
-    frames: this.anims.generateFrameNumbers("img_perso", { start: 5, end: 8 }), // on prend toutes les frames de img perso numerotées de 0 à 3
+    frames: this.anims.generateFrameNumbers("img_perso", { start: 27, end: 29 }), // on prend toutes les frames de img perso numerotées de 0 à 3
     frameRate: 10, // vitesse de défilement des frames
     repeat: -1 // nombre de répétitions de l'animation. -1 = infini
   }); 
@@ -84,13 +90,55 @@ function preload() {
     frameRate: 20
   });
 
+  /** CREATION DU CLAVIER **/  
+  clavier = this.input.keyboard.createCursorKeys();
+
+  /** GESTION DES INTERACTIONS ENTRE GROUPES ET ELEMENTS **/  
+  // chargement de la carte
+  const carteDuNiveau = this.add.tilemap("carte_natation");
 
 
-  /***********************************************************************/
-/** FONCTION UPDATE 
+  // chargement du jeu de tuiles
+  const tileset = carte_natation.addTilesetImage(
+        "tuiles_atlas2",  
+        "tuiles_natation",
+          //"tuiles_natation2",
+          
+        );  
+  // chargement du calque calque_background
+  const calque_background = carte_natation.createLayer(
+    "calque_background",
+    tileset
+  );
+    // chargement du calque calque_background_2
+    const Calque_Tuiles1 = carte_natation.createLayer(
+    "calque_background2",
+    tileset
+    );
+
+    // chargement du calque calque_plateformes
+    const claque_plateforme = carte_natation.createLayer(
+        "claque_plateforme",
+        tileset
+      );  
+    // définition des tuiles de plateformes qui sont solides
+    // utilisation de la propriété estSolide
+    claque_plateforme.setCollisionByProperty({ estSolide: true }); 
+    // ajout d'une collision entre le joueur et le calque plateformes
+    this.physics.add.collider(player, claque_plateforme); 
+
+    // redimentionnement du monde avec les dimensions calculées via tiled
+    this.physics.world.setBounds(0, 0, 3200, 640);
+    //  ajout du champs de la caméra de taille identique à celle du monde
+    this.cameras.main.setBounds(0, 0, 3200, 640);
+    // ancrage de la caméra sur le joueur
+    this.cameras.main.startFollow(player);  
+ }
+/***********************************************************************/
+/** METHODE UPDATE 
 /***********************************************************************/
 
-function update() {
+update() {
     if (gameOver) {
       return;
     }
@@ -112,3 +160,10 @@ function update() {
 
 
 }
+
+function chocAvecPyrahna(un_player, un_pyrahna) {
+    this.physics.pause();
+    player.setTint(0xff0000);
+    player.anims.play("anim_face");
+    gameOver = true;
+  }
